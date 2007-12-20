1ff896f7a6c2d0232784d4603e6dbc0cfacf6532
Kohsuke Kawaguchi <kk@kohsuke.org>
2007-12-20 18:59:35 +0000

    initial commit.

---
+++ src/main/java/org/jvnet/animal_sniffer/Main.java
@@ -0,0 +1,73 @@
+package org.jvnet.animal_sniffer;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.DataInputStream;
+import java.util.jar.JarFile;
+import java.util.jar.JarEntry;
+import java.util.Enumeration;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        if(args.length==0) {
+            System.err.println("Usage: java -jar animal-sniffer.jar [JAR/CLASS FILES]");
+            System.exit(-1);
+        }
+
+        for (String arg : args)
+            process(new File(arg));
+    }
+
+    private static void process(File file) throws IOException {
+        if(file.getName().endsWith(".class"))
+            processClassFile(file);
+        else
+        if(file.getName().endsWith(".jar"))
+            processJarFile(file);
+
+        // ignore other files
+    }
+
+    private static void processJarFile(File file) throws IOException {
+        JarFile jar = new JarFile(file);
+
+        Enumeration<JarEntry> e = jar.entries();
+        while (e.hasMoreElements()) {
+            JarEntry x =  e.nextElement();
+            if(!x.getName().endsWith(".class")) continue;
+            InputStream is = jar.getInputStream(x);
+            try {
+                process(file.getName()+':'+x.getName(),is);
+            } finally {
+                is.close();
+            }
+        }
+
+    }
+
+    private static void processClassFile(File file) throws IOException {
+        InputStream in = new FileInputStream(file);
+        try {
+            process(file.getPath(),in);
+        } finally {
+            in.close();
+        }
+    }
+
+    private static void process(String name, InputStream image) throws IOException {
+        DataInputStream dis = new DataInputStream(image);
+        byte[] buf = new byte[8];
+        dis.readFully(buf);
+
+        System.out.printf("%d.%d %s\n",u2(buf[6],buf[7]),u2(buf[4],buf[5]),name);
+    }
+
+    private static int u2(byte u, byte d) {
+        return ((int)u)*256+d;
+    }
+}
